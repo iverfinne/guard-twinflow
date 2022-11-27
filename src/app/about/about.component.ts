ac12a57a22b0684c5021a7ddde7ae6884725d5b7
iverfinne <iverfinne@gmail.com>
2022-11-27 03:05:51 +0100

    update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
 })
 
 export class AboutComponent implements OnInit {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
+
+
 export class AppComponent implements OnInit {
   name = 'Angular';
 
